58170c76d107dd44189e8458c87542d7348a7dc4
Manikandan1818 <simbumanii18@gmail.com>
2024-03-14 22:03:17 +0530

    User Model working on...

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,25 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
+
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
-const PORT = process.env.PORT || 8080;
+// MondoDBConnection
+const connect = () =>
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => {
+      console.log("Connected to Database...");
+    })
+    .catch((err) => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Server is running...");
+// ServerConnection & DB
+app.listen(8080, () => {
+  connect();
+  console.log("Connected to Server");
 });
-
-app.post("/signup", (req, res) => {
-  console.log(req.body);
-});
-
-app.listen(PORT, () => console.log("Server is running at port: " + PORT));
---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    confirmPassword: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
